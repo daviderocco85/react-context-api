3bfb2ee35dc53b54f3e480130026323df2b9bf19
daviderocco85 <roccodavide85@gmail.com>
2026-08-17 17:55:16 +0200

    feat: add Navbar component with budget mode toggle and navigation links

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import "./Navbar.css";
+import { NavLink } from "react-router";
+import { useContext } from "react";
+import { BudgetContext } from "../context/BudgetContext";
+
+export const Navbar = () => {
+    const { budgetMode, setBudgetMode } = useContext(BudgetContext);
+
+    return (
+        <nav className="navbar">
+            <NavLink to='/'>Homepage</NavLink>
+            <NavLink to='/about-us'>Chi siamo</NavLink>
+            <NavLink to='/products'>Prodotti</NavLink>
+            <button onClick={() => setBudgetMode(!budgetMode)}>
+                {budgetMode
+                    ? "Disattiva Modalità Budget"
+                    : "Attiva Modalità Budget"}
+            </button>
+        </nav>
+    );
+}
