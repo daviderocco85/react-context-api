83dedadaf9a6a877707f983ed927a6e3d976ce64
daviderocco85 <roccodavide85@gmail.com>
2026-08-17 18:34:54 +0200

    feat: filter products by budgetMode using BudgetContext

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,25 +1,33 @@
-import './Products.css'
+import './Products.css';
+import { useContext } from "react";
+import { BudgetContext } from "../context/BudgetContext";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 export const Products = () => {
     const [products, setProducts] = useState([]);
+    const { budgetMode } = useContext(BudgetContext);
 
     useEffect(() => {
         axios.get("https://fakestoreapi.com/products")
             .then(res => setProducts(res.data));
     }, []);
 
+    const filteredProducts = budgetMode
+        ? products.filter(product => product.price <= 30)
+        : products;
+
     return (
         <div className="products page">
             <h1>Prodotti</h1>
             <ul className="products-list">
-                {products.map(p => (
+                {filteredProducts.map(p => (
                     <li key={p.id} className="product-card">
                         <Link to={`/products/${p.id}`}>
                             <img src={p.image} alt={p.title} />
                             {p.title}
+                            <p>Prezzo: {p.price} €</p>
                         </Link>
                     </li>
                 ))}
